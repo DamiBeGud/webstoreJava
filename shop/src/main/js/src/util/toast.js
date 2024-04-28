3f6d0391cdec184104f2ca523a9ec120b7e3edfb
Damjan Mlinaric <dami.mlinaric@gmail.com>
2024-04-28 13:34:29 +0200

    toast should work now

--- shop/src/main/js/src/util/toast.js
+++ shop/src/main/js/src/util/toast.js
@@ -1,28 +1,32 @@
-export function toast(alertType, message) {
-    let alertDiv = document.createElement('div');
-    alertDiv.className = `alert alert-${alertType} alert-dismissible fade show m-3`;
-    alertDiv.setAttribute('role', 'alert');
-    alertDiv.style.cssText = ` position: sticky; bottom: 0px;left:100%; max-width:300px;height:60px; border-radius: 10px; z-index: 1000;`;
+export function toast (alertType, message) {
+  let bottomPosition = 0
+  let alertDiv = document.createElement('div')
+  let existingAlert = document.querySelectorAll('.alert')
+  if (existingAlert.length > 0) {
+    bottomPosition = existingAlert.length * 70
+  }
 
-    
-    alertDiv.innerHTML = `
+  alertDiv.className = `alert alert-${alertType} alert-dismissible fade show m-3`
+  alertDiv.setAttribute('role', 'alert')
+  alertDiv.style.cssText = `position: sticky; bottom: ${bottomPosition}px; left: 100%; max-width: 300px; height: 60px; border-radius: 10px; z-index: 1000;`
+
+  alertDiv.innerHTML = `
     <div>
     <span style="font-size: 1.2em; overflow: hidden; text-overflow: ellipsis; position: absolute; left: 5px;">${message}</span>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" style="background-color: transparent; border: none; position: absolute; right: 5px;">
         <span aria-hidden="true">&times;</span>
     </button>
     </div>
-    `;
+    `
+  let closeButton = alertDiv.querySelector('.btn-close')
+  closeButton.addEventListener('click', () => closeToast(alertDiv))
 
-    let closeButton = alertDiv.querySelector('.btn-close');
-    closeButton.addEventListener('click', () => closeToast(alertDiv));
-
-    document.body.appendChild(alertDiv);
+  document.body.appendChild(alertDiv)
 }
 
-function closeToast(alertDiv) {
-    alertDiv.classList.remove('show');
-    setTimeout(() => {
-        alertDiv.parentNode.removeChild(alertDiv);
-    }, 500);
+function closeToast (alertDiv) {
+  alertDiv.classList.remove('show')
+  setTimeout(() => {
+    alertDiv.parentNode.removeChild(alertDiv)
+  }, 500)
 }
